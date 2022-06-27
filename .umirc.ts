5510e1001f6d507bc5f67e93f660dbcbc35d7ae6
wangzhy <384650553@qq.com>
2022-06-27 18:52:22 +0800

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  dynamicImport: {},
   nodeModulesTransform: {
     type: 'none',
   },
+  layout: {},
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      exact: true,
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      exact: true,
+      path: '/home',
+      component: './home',
+      icon: 'project',
+      name: '首页',
+    },
+    {
+      exact: true,
+      path: '/chart',
+      component: './chart',
+      icon: 'areaChart',
+      name: '图表',
+    },
+    {
+      exact: true,
+      path: '/setting',
+      component: './setting',
+      icon: 'control',
+      name: '设置',
+    },
   ],
-  fastRefresh: {},
 });
